ab741dbe75436fd009ae1aee24b9516a10dc36bf
manoj <manoj.thadiparthi554@gmail.com>
2025-09-22 12:16:30 +0530

    add

--- public ip.tf
+++ public ip.tf
@@ -1,7 +1,7 @@
 resource "azurerm_public_ip" "mypip" {
   name                = "pip01"
-  resource_group_name = azurerm_resource_group.rg01.name
-  location            = azurerm_resource_group.rg01.location
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   allocation_method   = "Static"
 
   tags = {
