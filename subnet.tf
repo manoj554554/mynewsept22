bc846a4872b55cd7d72cb275ab53792fa18c72bb
manoj <manoj.thadiparthi554@gmail.com>
2025-09-22 12:43:20 +0530

    add

--- subnet.tf
+++ subnet.tf
@@ -1,5 +1,5 @@
 resource "azurerm_subnet" "sunet66" {
-  name                 = "mySubnet"
+  name                 = "mySubnet1"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.0.0/24"]
